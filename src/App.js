1b255a247d84f8a34cab00641c5521c7026ea072
jihyeon kil <jihyeon2085@gmail.com>
2022-01-03 16:07:01 +0900

    Create customer.js

--- src/App.js
+++ src/App.js
@@ -2,21 +2,53 @@
 import Customer from './component/Customer'
 import './App.css';
 
-const customer = {
+const customers = [
+  {
+  'id':1,
+  'image': 'https://placeimg.com/64/64/1',
   'name': "길지현",
   'birthday': "19920727",
   'univ':"한국대학교",
   'gender':"남성"
-}
+},
+{
+  'id':2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': "홍길동",
+  'birthday': "19930727",
+  'univ':"한국대학교",
+  'gender':"여성"
+},
+{
+  'id':3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': "이순신",
+  'birthday': "19940727",
+  'univ':"한국대학교",
+  'gender':"남성"
+},
+]
 
 
 function App() {
   return (
-    <Customer
-    name={customer.name}
-    birthday={customer.birthday}
-    univ ={customer.univ}
-    gender={customer.gender}/>
+    <div>
+    {
+      customers.map((item) => {
+        return(
+          <Customer
+          key = {item.id}
+          id={item.id}
+          image ={item.image}
+          name ={ item.name}
+          birthday ={item.birthday}
+          univ = {item.univ}
+          gender= {item.gender}
+          />
+        )
+      } )
+    }
+    </div>
   );
 }
 
--- src/component/Customer.js
+++ src/component/Customer.js
@@ -3,7 +3,25 @@ import React from 'react';
 function Customer(props){
     return(
         <div>
-            <h2>{props.name}</h2>
+            <CustomerProfile id={props.id} image={props.image} name={props.name}/>
+            <CustomerInfo birthday={props.birthday} univ={props.univ} gender={props.gender} />
+        </div>
+    )
+}
+
+
+function CustomerProfile(props){
+    return(
+        <div>
+            <img src={props.image} alt="profile"/>
+            <h2>{props.name}({props.id})</h2>
+        </div>
+    )
+}
+
+function CustomerInfo(props){
+    return(
+        <div>
             <p>{props.birthday}</p>  
             <p>{props.univ}</p>  
             <p>{props.gender}</p>  
